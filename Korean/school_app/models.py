f9b5361020a5b4d406b84fe0079219b1b0838309
tr1viall <666d.aks666@gmail.com>
2025-09-29 10:52:01 +0500

    Korean

--- Korean/school_app/models.py
+++ Korean/school_app/models.py
@@ -1,18 +1,36 @@
 from django.db import models
 
 class student(models.Model):
-    name = models.CharField("ФИО", max_length=100)
+    name = models.CharField("Имя", max_length=100)
+    surname = models.CharField("Фамилия", max_length=100)
+    patronymic = models.CharField("Отчество", max_length=100)
     phone = models.CharField("Телефон", max_length=20)
-    email = models.CharField("Эл.почта", max_length=100)
+    email = models.EmailField("Эл.почта", max_length=100)
     registration_date = models.DateField("Дата регистрации")
     document = models.CharField("Документы", max_length=100)
     level = models.CharField("Уровень", max_length=50)
+    
+    class Meta:
+        verbose_name_plural = "Студенты"
+        verbose_name = "Студент"
+
+    def __str__(self): 
+        return f"{self.name} {self.surname}"
 
 class teacher(models.Model):
-    name = models.CharField("ФИО", max_length=100)
+    name = models.CharField("Имя", max_length=100)
+    surname = models.CharField("Фамилия", max_length=100)
+    patronymic = models.CharField("Отчество", max_length=100)
     phone = models.CharField("Телефон", max_length=20)
-    email = models.CharField("Эл.почта", max_length=100)
+    email = models.EmailField("Эл.почта", max_length=100)
     specialization = models.CharField("Специализация", max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Преподаватели"
+        verbose_name_plural = "Преподаватель"
+
+    def __str__(self): 
+        return f"{self.name} {self.surname}"
 
 class course(models.Model):
     enrollment_date = models.DateField("Дата записи")
@@ -21,15 +39,39 @@ class course(models.Model):
     level = models.CharField("Уровень", max_length=100)
     description = models.TextField("Описание", max_length=100)
 
+    class Meta:
+        verbose_name_plural = "Курсы"
+        verbose_name = "Курс"
+
+    def __str__(self): 
+        return f"{self.name}" 
+
 class lessons(models.Model):
     title = models.CharField("Название", max_length=50)
     content = models.TextField("Содержание", max_length=20)
     date = models.DateField("Дата")
     course_id = models.ForeignKey(course, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "Занятия"
+        verbose_name = "Занятие"
+
+
+    def __str__(self): 
+        return f"{self.title}"
+
 class enrollments(models.Model):
     enrollment_date = models.DateField("Дата записи")
     course_id = models.ForeignKey(course, on_delete=models.CASCADE) 
     student_id = models.ForeignKey(student, on_delete=models.CASCADE)
     progress = models.DecimalField("Процент прохождения курса", max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = "Записи на занятия"
+        verbose_name = "Запись на занятие"
+
+
+    def __str__(self): 
+        return f"{self.id}"
+    
 
